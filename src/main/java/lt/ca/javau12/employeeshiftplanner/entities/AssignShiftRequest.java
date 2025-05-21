7fa7eff128f307a77499d63339c66701b91b4ad7
Matas313 <matas.kairiukstis@gmail.com>
2025-05-21 16:05:01 +0300

    Pridėta shift assign funkcija ir kiti pataisymai

---
+++ src/main/java/lt/ca/javau12/employeeshiftplanner/entities/AssignShiftRequest.java
@@ -0,0 +1,23 @@
+package lt.ca.javau12.employeeshiftplanner.entities;
+
+public class AssignShiftRequest {
+
+    private Long shiftId;
+    private Long employeeId;
+
+    public Long getShiftId() {
+        return shiftId;
+    }
+
+    public void setShiftId(Long shiftId) {
+        this.shiftId = shiftId;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+}
